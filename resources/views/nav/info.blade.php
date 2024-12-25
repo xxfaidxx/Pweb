97445eaa21dd28a2a7a9f2e0f066065a9035212d
xxfaidxxx <faidwijaya@gmail.com>
2024-12-26 00:14:33 +0700

    Minimize Code

--- resources/views/nav/info.blade.php
+++ resources/views/nav/info.blade.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/content.css">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="tabs">
-        <button class="tab" onclick="loadContent('/dokumen')">Dokumen</button>
-        <button class="tab" onclick="loadContent('/berita')">Berita / Informasi
-            Seputar PPDB 2024</button>
-        <button class="tab active" onclick="loadContent('/info')">Info Praktikal</button>
-    </div>
-</body>
-
-</html>
+<div class="tabs">
+    <button class="tab" onclick="loadContent('/dokumen')">Dokumen</button>
+    <button class="tab" onclick="loadContent('/berita')">Berita / Informasi
+        Seputar PPDB 2024</button>
+    <button class="tab active" onclick="loadContent('/info')">Info Praktikal</button>
+</div>
