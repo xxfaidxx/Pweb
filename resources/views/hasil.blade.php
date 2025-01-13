26ac38d1bb65071f74b9624bddfc35da7d1b6711
xxfaidxxx <faidwijaya@gmail.com>
2025-01-13 23:09:51 +0700

    Modify

--- resources/views/hasil.blade.php
+++ resources/views/hasil.blade.php
@@ -10,31 +10,30 @@
                 <button class="tab" onclick="loadContent('/smp')">SMP</button>
                 <button class="tab" onclick="loadContent('/sma')">SMA</button>
             </div>
-            <table border="1" cellpadding="5" cellspacing="0">
+            <table border="1" cellpadding="15" cellspacing="0" cellmargin="2" width="100%" style="margin: 0 auto;">
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Created At</th>
-                        <th>Asal Sekolah</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Jenjang</th> <!-- Menambahkan kolom Jenjang -->
+                        <th style="text-align: center;">Name</th>
+                        <th style="text-align: center;">Email</th>
+                        <th style="text-align: center;">Created At</th>
+                        <th style="text-align: center;">Asal Sekolah</th>
+                        <th style="text-align: center;">Jenis Kelamin</th>
+                        <th style="text-align: center;">Jenjang</th> <!-- Menambahkan kolom Jenjang -->
                     </tr>
                 </thead>
                 <tbody>
                     <!-- Loop untuk menampilkan data pengguna -->
                     @foreach ($users as $user)
                         <tr>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>{{ $user->created_at }}</td>
-                            <td>{{ $user->asal_sekolah }}</td>
-                            <td>{{ $user->jenis_kelamin }}</td>
-                            <td>{{ $user->jenjang }}</td> <!-- Menampilkan Jenjang -->
+                            <td style="text-align: center;">{{ $user->name }}</td>
+                            <td style="text-align: center;">{{ $user->email }}</td>
+                            <td style="text-align: center;">{{ $user->created_at }}</td>
+                            <td style="text-align: center;">{{ $user->asal_sekolah }}</td>
+                            <td style="text-align: center;">{{ $user->jenis_kelamin }}</td>
+                            <td style="text-align: center;">{{ $user->jenjang }}</td> <!-- Menampilkan Jenjang -->
                         </tr>
                     @endforeach
                 </tbody>
             </table>
-        </div>
-    @endsection
+        @endsection
 </body>
